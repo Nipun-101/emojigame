1b846e6612ed40f14d7ec2ac37ded785856e8b46
Nipun-101 <nipundileep101@gmail.com>
2022-07-16 07:09:24 +0000

    first push emoji game

---
+++ src/components/EmojiGame/index.js
@@ -0,0 +1,10 @@
+import {Component} from 'react'
+import './index.css'
+
+class EmojiGame extends Component {
+  render() {
+    return <div className="bg-container">df</div>
+  }
+}
+
+export default EmojiGame
